9c7a052240b0890ed33ccd434440e1ca681e3aac
sunny_town的博客技术支持 <13997766336@163.com>
2024-05-31 21:01:29 +0800

    Update shijuapi.js

--- shijuapi.js
+++ shijuapi.js
@@ -9,20 +9,16 @@ if(!file_exists($filename)) {
     die($filename . ' 数据文件不存在');
 }
  
-$data = array();
+// 读取整个数据文件
+$data = file_get_contents($filename);
  
-// 打开文档
-$fh = fopen($filename, 'r');
- 
-// 逐行读取并存入数组中
-while (!feof($fh)) {
-    $data[] = fgets($fh);
-}
- 
-// 关闭文档
-fclose($fh);
+// 按换行符分割成数组
+$data = explode(PHP_EOL, $data);
  
 // 随机获取一行索引
 $result = $data[array_rand($data)];
+ 
+// 去除多余的换行符（保险起见）
+$result = str_replace(array("\r","\n","\r\n"), '', $result);
  
 echo $result;
